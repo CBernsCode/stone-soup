0765bc8202df50ce4fadddbe2976f63894f33678
Chris Berns <cberns223@gmail.com>
2017-11-04 17:48:08 -0400

    Basic front end components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Chat from './Chat.js';
+import Recipes from './Recipes.js';
 import './App.css';
 
 import Footer from './Footer.js';
@@ -18,9 +19,67 @@ class App extends Component {
         </p>
         <Chat />
         <Footer />
+        <div className="container">
+          <Recipes recipes={recipes}/>
+          <Chat messages={msgs}/>
+        </div>
       </div>
     );
   }
 }
+
+var msgs = [{
+  message: "Hello world",
+  time: "123"
+},
+{
+  message: "Hello world",
+  time: "124"
+},
+{
+  message: "Hello world",
+}
+
+]
+
+var recipes = [
+  {
+    name: "Cake",
+    ingredients: [
+      {
+        quantity: 2,
+        name: "eggs"
+      },
+      {
+        quantity: "3lbs",
+        name: "Flour"
+      }
+    ]
+  },
+  {
+    name: "Eggs",
+    ingredients: [
+      {quantity: 2,
+      name: "eggs"
+      }
+    ]
+  },
+  {
+    name: "Eggs",
+    ingredients: [
+      {quantity: 2,
+      name: "eggs"
+      }
+    ]
+  }
+  ,{
+    name: "Eggs",
+    ingredients: [
+      {quantity: 2,
+      name: "eggs"
+      }
+    ]
+  }
+]
 
 export default App;
