9a8384af979732d5044ab4dc573a81adc74ba1c4
Chris Berns <cberns223@gmail.com>
2017-11-04 15:15:52 -0400

    Update with Chat

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Chat from './Chat.js';
 import './App.css';
 
 class App extends Component {
@@ -8,11 +9,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Stone Soup</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Chat />
       </div>
     );
   }
---
+++ src/Chat.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Chat extends Component{
+  render(){
+    return (
+      <div>
+        <h2>Chat</h2>
+      </div>
+    )
+  }
+
+}
+
+export default Chat;
