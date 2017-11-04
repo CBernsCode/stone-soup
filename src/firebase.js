a9d43865242f342a54ab91283bb56fa51c5e49e1
Ross <RossHall251@outlook.com>
2017-11-04 15:25:38 -0400

    Firebase Config

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/4.6.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA4ZX2vIBTM3ch2lUNZFY1MMrCQSJR687s",
+    authDomain: "stone-soup-cd649.firebaseapp.com",
+    databaseURL: "https://stone-soup-cd649.firebaseio.com",
+    projectId: "stone-soup-cd649",
+    storageBucket: "stone-soup-cd649.appspot.com",
+    messagingSenderId: "1081040749799"
+  };
+  firebase.initializeApp(config);
+</script>
