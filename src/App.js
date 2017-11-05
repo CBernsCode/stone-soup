207bade949d26bd3951c4d906cc0449a35c4b079
MarvinF <marvin.fung@ymail.com>
2017-11-04 21:35:34 -0400

    we eat the booty like groceries

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import logo from './logo.svg';
 import Chat from './Chat.js';
 import './App.css';
 
+import Footer from './Footer.js';
+import Header from './Header.js';
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Stone Soup</h1>
-        </header>
+        <div className="col-sm-1"></div>
+        <Header />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <Chat />
+        <Footer />
       </div>
     );
   }
---
+++ src/Sidebar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Sidebar.css';
+
+class Sidebar extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default Sidebar;
