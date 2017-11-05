de9bb558e29e1e2910a61ef3a697cd83f2011b10
AmyMazzucotelli <amazzucotelli@gmail.com>
2017-11-05 03:07:57 -0500

    changed recipes to static array

--- src/Recipes.js
+++ src/Recipes.js
@@ -2,51 +2,15 @@ import React, { Component } from 'react';
 import './Recipes.css'
 import RecipeProvider from './RecipeProvider.js'
 
-var obj = [
-  {
-    name: "some name",
-    ingredients: {
-      quantity: 2,
-      name: "egg"
-    }
-  },
-  {
-    name: "some name",
-    ingredients: {
-      quantity: 2,
-      name: "egg"
-    }
-  }
-]
-
 class Recipes extends Component{
-  constructor() {
-    super();
-    var rProv = new RecipeProvider();
-    this.state = {
-      recipes: []
-    }
-  }
-  componentDidMount(){
 
-    var rProv = new RecipeProvider();
-    rProv.getRecipe("-Ky8MbZFcEOQjBD3dPwV").then((result) => {
-      this.setState({recipe: result})}
-    ).then(() => {console.log(this.state.recipe)}
-  )
-    
-  }
   render(){
 
       return (
         <section  className="col-md-9">
             <h2>Recipes</h2>
             <div id="recipes">
-              {this.props.recipes.map((x) => {
-                return (
-                  <Recipe img={x.img} name={x.name} ingredients={x.ingredients}/>
-                )
-              })}
+              
             </div>
         </section>
       )
