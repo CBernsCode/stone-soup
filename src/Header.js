3b8ba90b9e99774603331fea8a5f97a4f47ce5ac
MarvinF <marvin.fung@ymail.com>
2017-11-04 21:35:34 -0400

    Added footer and header, and skeleton of sidebar

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return(
+      <div className="page-header">
+        <div className="col-sm-1"></div>
+        <h1>Stone Soup</h1>
+      </div>
+    );
+  }
+}
+
+
+export default Header;
