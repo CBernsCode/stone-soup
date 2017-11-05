30fe11029eeb73cd7c2db80cb89884d1dcdb61ae
AmyMazzucotelli <amazzucotelli@gmail.com>
2017-11-05 01:39:31 -0500

    added getImageURL function to RecipeProvider

--- src/RecipeProvider.js
+++ src/RecipeProvider.js
@@ -22,6 +22,21 @@ class RecipeProvider{
     });
   }
 
+  // Gets imageURL from ID
+  // Returns a Promise, as usual
+  getImageURL(id){
+    return new Promise((resolve, reject) => {
+      this.ref.child(id).once('value', (snapshot) => {
+        if(snapshot.val().imageURL){
+          resolve(snapshot.val().imageURL);
+        } else {
+          // no image URL found, set to Lobster Roll
+          resolve("http://www.seriouseats.com/images/2016/12/20161208-sous-vide-lobster-59-lobster-roll-duo.jpg");
+        }
+      }
+    )});
+  }
+
 
   // Send a recipe to the database
   // Returns Firebase's randomly generated key
